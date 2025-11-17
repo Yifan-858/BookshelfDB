6f0da759db77155a467eb64a5d51e6a545f60c03
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2025-11-17 21:07:30 +0100

    Add project files.

---
+++ BookshelfDB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BookshelfDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
