7b35c5bffb5db4c2217690479230949004e4fddb
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2025-11-17 22:40:44 +0100

    WIP

---
+++ BookshelfDB/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookshelfDB.Model;
+
+
+namespace BookshelfDB.Data
+{
+    public class AppDbContext : DbContext
+    {   
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
---
+++ BookshelfDB/Model/Book.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookshelfDB.Model
+{
+    public class Book
+    {
+        private string _id;
+        public string Id 
+        { 
+            get => _id;
+            set 
+            {
+                if(value.Length != 13)
+                {
+                    throw new ArgumentException("ISBN must be 13 digits");
+                }
+            } 
+        }
+        public string Title { get; set; }
+        public Language Language { get; set; }
+        public double Price { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorID { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public enum Language
+    {
+        English,Swedish,French,Italian
+    }
+}
